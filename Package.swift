348c7b5061826ccf2b0bcfd649f59baf434b3f78
bimawa <bimawa@gmail.com>
2022-08-16 21:44:34 +0500

    fix(spm): spm adopting

--- Package.swift
+++ Package.swift
@@ -1,22 +1,32 @@
-// swift-tools-version:5.0
+// swift-tools-version: 5.6
+// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "GPUImage",
     platforms: [
-        .macOS(.v10_11), .iOS(.v9),
+           .macOS(.v10_13), .iOS(.v13),
     ],
     products: [
+        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "GPUImage",
             targets: ["GPUImage"]),
     ],
-    dependencies: [],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+    ],
     targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "GPUImage",
-            path: "framework/Source",
-            exclude: ["Linux", "Operations/Shaders"])],
-    swiftLanguageVersions: [.v4_2]
+            dependencies: [],
+            exclude: ["Metal"],
+            resources: [
+                .process("Resources")
+            ])
+    ]
 )
